3ed9d70bf094951b5bed23ee4c161ef6d95cb5fd
nidnogg <henriquevt98@gmail.com>
2024-03-05 00:53:01 -0300

    Added main patchwork js script

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
-// Compare date
-const dayHasElapsed = () => {
-  const timestamp = new Date('2024-03-04T23:59:59Z');
-  const currentDate = new Date();
-  console.log(currentDate)
-  // Set currentDate to exactly 24 hours ago
-  currentDate.setDate(currentDate.getDate() - 1);
-  
-  // Check if the timestamp is from a day ago or not
-  return timestamp >= currentDate
+const fs = require('fs');
+const path = require('path');
+
+// File paths
+const lastFetchAtPath = '/Users/nidnogg/localdev/expensio-meter/data/last_fetch_at.json';
+const currencyDataPath = '/Users/nidnogg/localdev/expensio-meter/data/currency_data.json';
+
+const writeJson = (filePath, fileContent) => {
+  fs.writeFile(filePath, fileContent, (err) => {
+    if (err) {
+      console.error('Error creating file:', err);
+      return;
+    }
+    console.log(`File created successfully at ${filePath}`);
+  });
 }
 
-// Main routine
-fetch('https://raw.githubusercontent.com/nidnogg/expensio-meter/main/data/currency_data.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+const dayHasElapsed = (dateToCheck) => {
+  const timestamp = new Date(dateToCheck);
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+
+  return timestamp < currentDate;
+}
+
+// Main routine.
+fs.readFile(lastFetchAtPath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading last fetch date file:', err);
+    return;
+  }
+  
+  try {
+    const lastFetchAtData = JSON.parse(data);
+
+    if (!dayHasElapsed(lastFetchAtData.date)) { 
+      process.exit(1); 
+    } else {
+      // Fetch currency data
+      fetch('https://raw.githubusercontent.com/nidnogg/expensio-meter/main/data/currency_data.json')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(currencyData => {
+          const newLastFetchAtDate = { date: new Date() };
+          writeJson(currencyDataPath, JSON.stringify(currencyData, null, 2));
+          writeJson(lastFetchAtPath, JSON.stringify(newLastFetchAtDate, null, 2));
+          process.exit(0);
+        })
+        .catch(error => {
+          console.error('There was a problem fetching the data:', error);
+        });
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log(dayHasElapsed(data.meta.last_updated_at));
-  })
-  .catch(error => {
-    console.error('There was a problem fetching the data:', error);
-  });
+  } catch (error) {
+    console.error('Error parsing last fetch date JSON:', error);
+  }
+});
